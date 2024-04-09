5e58a568a42858f9a62afbb50ff454649153d79a
reza gholamalitabar <reza.gh.fi@gmail.com>
2024-04-09 13:53:20 +0330

    complete i/o and cpu/bound

--- 0_intro/main.py
+++ 0_intro/main.py
@@ -1,2 +1,37 @@
 # import asyncio for use asyncio
-import asyncio
+import requests
+# یکی سری پردازش ها محدود به پردازنده هستند و یکسری از آن ها محدود به ورودی و خروجی هستند
+# CPU - bound VS I/O bound
+
+# یک برنامه می نویسیم که درخواست به سایت گیت هاب می فرسته و هدر بسته ها رو در فایلی ذخیره کنیم.
+
+# I/O bound
+response = requests.get('https://github.com/rezaghfi')
+
+#dictionary
+#CPU - bound
+items = response.headers.items()
+
+#list
+# CPU - bound
+res_headers = [f"{key} : {value}" for key, value in items]
+
+# I/O - bound
+for item in items:
+    print(item)
+# I/O - bound
+print("---------------------------------------")
+for item in res_headers:
+    print(item)
+
+#print(type(res_headers))
+# CPU - bound
+formatted_headers = str(res_headers)
+
+# I/O bound
+with open("headers.txt","w") as file:
+    file.write(formatted_headers)
+
+formatted_headers = "\n".join(res_headers)
+with open("headers.txt","a") as file:
+    file.write(formatted_headers)
